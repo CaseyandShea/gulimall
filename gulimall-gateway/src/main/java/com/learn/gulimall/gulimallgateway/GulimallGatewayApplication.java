20e79e96c0c7c305594cc2d74e0db7ed64d11ade
Casey <casey@flashexpress.com>
2020-04-14 12:00:52 +0800

    gateway 测试

--- gulimall-gateway/src/main/java/com/learn/gulimall/gulimallgateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/learn/gulimall/gulimallgateway/GulimallGatewayApplication.java
@@ -2,8 +2,11 @@ package com.learn.gulimall.gulimallgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient //开启网关服务的注册发现
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {
